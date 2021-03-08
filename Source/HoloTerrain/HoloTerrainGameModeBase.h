e74047d1e30075202ce4801403ecf3ba62cac34e
Jason Ioffe <JIoffe@users.noreply.github.com>
2021-03-07 19:52:55 -0500

    It ... runs.

---
+++ Source/HoloTerrain/HoloTerrainGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HoloTerrainGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOLOTERRAIN_API AHoloTerrainGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
